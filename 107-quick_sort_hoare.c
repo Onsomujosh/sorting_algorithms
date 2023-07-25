711c1db4a03f184d3afefa1228b8bebf742858e6
DevWambugu <venusdata100@gmail.com>
2023-07-25 04:29:34 -0700

    comply with betty

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -14,21 +14,19 @@ void swap_ints(int *a, int *b)
 	*b = tmp;
 }
 
-/**
-*This function implements the hoare partion scheme
-*used in quick sort algorithm. To partition
-*this function will take in an integer array
-*its size and 2 indices low and high
-*alternatively, you can use left and right
-*/
-int hoare_partition(int *array, size_t size, int low, int high);
+/*This function implements the hoare partion scheme*/
+/*used in quick sort algorithm. To partition*/
+/*this function will take in an integer array*/
+/*its size and 2 indices low and high*/
+/*alternatively, you can use left and right*/
+int hoare_partition(int *array, size_t size, int left, int right);
 /**
 *hoare_partition - this function implements the hoare
 *	partition on an array
 *@array: This is the integer array to be sorted
 *@size: this is the size of the array
-*@low:  this refers to the left  most side of the arrray
-*@high: this refers to the right most side of the array
+*@left:  this refers to the left  most side of the arrray
+*@right: this refers to the right most side of the array
 *
 *Return: This function returns the value of high.
 *	this is the position of the pivot after partioning
@@ -37,16 +35,16 @@ int hoare_partition(int *array, size_t size, int left, int right)
 {
 	int pivot_integer, above_pivot, below_pivot;
 
-	/*for this task, the take the pivot as the
-	*most right sided element in the arrray*/
+	/*for this task, the take the pivot as the*/
+	/*most right sided element in the arrray*/
 	pivot_integer = array[right];
-	/*use 2 indices above_pivot and below_pivot
-	*to move towards each other until they cross*/
+	/*use 2 indices above_pivot and below_pivot*/
+	/*to move towards each other until they cross*/
 	for (above_pivot = left - 1, below_pivot = right + 1;
 		above_pivot < below_pivot;)
 	{
-		/*if above_pivot is less than below_pivot
-		*swap the 2 elements and continue*/
+		/*if above_pivot is less than below_pivot*/
+		/*swap the 2 elements and continue*/
 		do {
 			above_pivot++;
 		} while (array[above_pivot] < pivot_integer);
@@ -62,14 +60,14 @@ int hoare_partition(int *array, size_t size, int left, int right)
 		}
 	}
 
-	/*return the value of the position of the
-	*pivot element after partioning*/
+	/*return the value of the position of the*/
+	/*pivot element after partioning*/
 	return (above_pivot);
 }
 
-/**Next, create a function that will implement the quicksort
-*algorithm using recursion. This function will implement
-*the hoare partitiion and quicksort techniques*/
+/*Next, create a function that will implement the quicksort*/
+/*algorithm using recursion. This function will implement*/
+/*the hoare partitiion and quicksort techniques*/
 void hoare_sort(int *array, size_t size, int left, int right);
 
 /**
@@ -83,15 +81,15 @@ void hoare_sort(int *array, size_t size, int left, int right);
 */
 void hoare_sort(int *array, size_t size, int left, int right)
 {
-	/*define an integer varaible that stores the position
-	*of the pivot element after the hoare partion*/
+	/*define an integer varaible that stores the position*/
+	/*of the pivot element after the hoare partion*/
 	int partion_position;
 
-	/*cfreate a base case for the recursion
-	*check if the subarray on the left and
-	*right contains more than one element
-	*if, it doesn't, it means the subarray
-	*can be further partioned and sorted*/
+	/*cfreate a base case for the recursion*/
+	/*check if the subarray on the left and*/
+	/*right contains more than one element*/
+	/*if, it doesn't, it means the subarray*/
+	/*can be further partioned and sorted*/
 	if (right - left > 0)
 	{
 		/*call tyhe partition function to partion the subarray*/
